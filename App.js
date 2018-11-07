cbc74d14ccff00af094ed8ecd6878352c476534f
magga <magga@theloudminority.co.id>
2018-11-07 21:23:45 +0700

    Pertemuan 19 part 2

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import HomeScreen from './src/screens/HomeScreen';
+import reducers from './src/reducers/index.js';
 
 const AppNavigator = createStackNavigator({
 	home: HomeScreen
@@ -14,7 +14,7 @@ const AppNavigator = createStackNavigator({
 class App extends Component {
     render() {
         return (
-			<Provider store={createStore()}>
+			<Provider store={createStore(reducers)}>
 				<View style={{ flex: 1 }}>
 					<AppNavigator />
 				</View>
