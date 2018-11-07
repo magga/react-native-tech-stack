724a6a5f6d7d5bfddeec40fb626eeddefd6f1c6c
magga <magga@theloudminority.co.id>
2018-11-07 22:14:02 +0700

    Pertemuan 19 part 5

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
 class HomeScreen extends Component {
     render() {
+        console.log('ini local libraries', this.props.local_libraries);
+
         return (
             <View>
                 <Text>HomeScreen</Text>
@@ -11,4 +14,11 @@ class HomeScreen extends Component {
     }
 }
 
-export default HomeScreen;
+const mapStateToProps = (state) => {
+    return {
+        local_libraries: state.libraries,
+        cart: state.cart
+    };
+};
+
+export default connect(mapStateToProps)(HomeScreen);
