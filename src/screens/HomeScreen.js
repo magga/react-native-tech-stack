edcdcdad1c443619eed1a356c382c5351870eb0e
magga <magga@theloudminority.co.id>
2018-11-12 21:26:53 +0700

    Pertemuan 21 part 3

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,22 +4,6 @@ import { Card, CardItem } from 'native-base';
 import { connect } from 'react-redux';
 
 class HomeScreen extends Component {
-    _renderLibraries() {
-        return this.props.local_libraries.map((item, index) => {
-            return (
-                <Card key={index}>
-                    <CardItem>
-                        <Text>{item.title}</Text>
-                    </CardItem>
-
-                    <CardItem>
-                        <Text>{item.description}</Text>
-                    </CardItem>
-                </Card>
-            );
-        });
-    }
-
     render() {
         return (
             <View>
@@ -27,12 +11,21 @@ class HomeScreen extends Component {
                     data={this.props.local_libraries}
                     renderItem={(data) => {
                         return (
-                            <Text style={{ fontSize: 40 }}>{data.item.description}</Text>
+                            <Card>
+                                <CardItem>
+                                    <Text>{data.item.title}</Text>
+                                </CardItem>
+
+                                <CardItem>
+                                    <Text>{data.item.description}</Text>
+                                </CardItem>
+                            </Card>
                         );
                     }}
                     keyExtractor={(item) => {
                         return item.id.toString();
                     }}
+                    
                 />
             </View>
         );
