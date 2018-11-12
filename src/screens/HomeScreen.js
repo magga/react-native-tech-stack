15c7d20d13716ceb55e6eb99fd0afc19c3d9825b
magga <magga@theloudminority.co.id>
2018-11-12 20:10:52 +0700

    Pertemuan 21 part 1

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card, CardItem } from 'native-base';
 import { connect } from 'react-redux';
 
 class HomeScreen extends Component {
-    render() {
-        console.log('ini local libraries', this.props.local_libraries);
+    _renderLibraries() {
+        return this.props.local_libraries.map((item, index) => {
+            return (
+                <Card key={index}>
+                    <CardItem>
+                        <Text>{item.title}</Text>
+                    </CardItem>
 
+                    <CardItem>
+                        <Text>{item.description}</Text>
+                    </CardItem>
+                </Card>
+            );
+        });
+    }
+
+    render() {
         return (
-            <View>
-                <Text>HomeScreen</Text>
-            </View>
+            <ScrollView>
+                {this._renderLibraries()}
+            </ScrollView>
         );
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        local_libraries: state.libraries,
-        cart: state.cart
+        local_libraries: state.libraries
     };
 };
 
