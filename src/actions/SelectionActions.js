0b5c59ae7c5921ca5db7875a4217ee01d8e8c57d
magga <magga@theloudminority.co.id>
2018-11-26 20:42:34 +0700

    Pertemuan 23 part 1

--- src/actions/SelectionActions.js
+++ src/actions/SelectionActions.js
@@ -1,6 +1,17 @@
 export const selectStackId = (id) => {
+    // BUSINESS LOGIC
+    // FETCHING DATA
+
     return {
         type: 'SELECT_STACK_ID',
         payload: id
     };
 };
+
+// FUNCTION ADD PRODUCT
+
+// FUNCTION CHANGE QUANTITY
+
+// FUNCTION REMOVE PRODUCT
+
+// FUNCTION SELECT ADDRESS
